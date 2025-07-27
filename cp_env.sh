821cd892ea14fb567c2f3cc57fad59d901242105
thaodtp <dtpthao.its@gmail.com>
2025-07-27 13:50:54 +0700

    minor update

--- cp_env.sh
+++ cp_env.sh
@@ -1,3 +1,5 @@
-cp auth-server/.env.dev auth-server/.dev
+cp auth-server/.env.dev auth-server/.env
 cp forward-proxy/.env.dev forward-proxy/.env
 cp reverse-proxy/.env.dev reverse-proxy/.env
+
+# todo add command to upload reverse-proxy's ntor certificate
